6240a07fb8861deaed38eb3990c85af4d51c9f30
AftonSlone <afton.slone@gmail.com>
2021-10-17 01:14:01 -0400

    Updates to Business Styling

--- frontend/src/components/Business/index.js
+++ frontend/src/components/Business/index.js
@@ -1,3 +1,4 @@
+import "./Business.css";
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
@@ -21,6 +22,14 @@ export default function Business() {
   const [rating, setRating] = useState(5);
   const [text, setText] = useState("dog133");
   const [errors, setErrors] = useState(null);
+
+  const reviewScore = (business) => {
+    let count = 0;
+    business.Reviews.forEach((review) => {
+      count += review.rating;
+    });
+    return count / business.Reviews.length;
+  };
 
   const editReview = async (e) => {
     const newReview = {
@@ -57,9 +66,68 @@ export default function Business() {
 
   if (!currentBusiness) return "Loading...";
   return (
-    <div>
+    <div className="businessWrapper">
       <Header />
-      {currentBusiness.name}
+      <div className="businessBanner"></div>
+      <div className="businessContent">
+        <h2>{currentBusiness.name}</h2>
+        <div className="businessCardReviewScore">
+          <div
+            className={`businessCardReviewScoreIcon ${
+              1 <= reviewScore(currentBusiness)
+            }`}
+          >
+            <i className="fas fa-star"></i>{" "}
+          </div>
+          <div
+            className={`businessCardReviewScoreIcon ${
+              2 <= reviewScore(currentBusiness)
+            }`}
+          >
+            <i className="fas fa-star"></i>
+          </div>
+          <div
+            className={`businessCardReviewScoreIcon ${
+              3 <= reviewScore(currentBusiness)
+            }`}
+          >
+            <i className="fas fa-star"></i>
+          </div>
+          <div
+            className={`businessCardReviewScoreIcon ${
+              4 <= reviewScore(currentBusiness)
+            }`}
+          >
+            <i className="fas fa-star"></i>
+          </div>
+          <div
+            className={`businessCardReviewScoreIcon ${
+              5 === reviewScore(currentBusiness)
+            }`}
+          >
+            <i className="fas fa-star"></i>
+          </div>
+        </div>
+        <div className="businessCardAddress">
+          {currentBusiness.BusinesType.name} - {currentBusiness.City.name},{" "}
+          {currentBusiness.State.name}
+        </div>
+        <div className="businessLocationHours">
+          <h2>{`Location & Hours`}</h2>
+          <p>Sun: {currentBusiness.Hour.sunday}</p>
+          <p>Mon: {currentBusiness.Hour.monday}</p>
+          <p>Tue: {currentBusiness.Hour.tuesday}</p>
+          <p>Wed: {currentBusiness.Hour.wednesday}</p>
+          <p>Thu: {currentBusiness.Hour.thursday}</p>
+          <p>Fri: {currentBusiness.Hour.friday}</p>
+          <p>Sat: {currentBusiness.Hour.saturday}</p>
+        </div>
+        <div className="businessAbout">
+          <h2>{`About the Business`}</h2>
+          <p>{currentBusiness.info}</p>
+        </div>
+      </div>
+      {/* {currentBusiness.name}
       {currentBusiness.Reviews.map((review) => (
         <div key={review.id}>
           <h2>{review.text}</h2>
@@ -87,6 +155,7 @@ export default function Business() {
       <button className="HeaderBtnSignup" onClick={postReview}>
         New Review
       </button>
+    </div> */}
     </div>
   );
 }
