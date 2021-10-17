247be18feb1cfcac5fc0277c5544c479c58cd2b2
AftonSlone <afton.slone@gmail.com>
2021-10-17 18:54:02 -0400

    Working on Business Edit

---
+++ frontend/src/components/EditBusinessForm/index.js
@@ -0,0 +1,141 @@
+import "./EditBusinessForm.css";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getStates } from "../../store/state";
+import { getTypes } from "../../store/type";
+import { getCities } from "../../store/city";
+import { newBusiness } from "../../store/business";
+
+export default function EditBusinessForm({ setEditBusinessModal }) {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.session.user);
+  const states = useSelector((state) => state.state);
+  const cities = useSelector((state) => state.city);
+  const types = useSelector((state) => state.type);
+
+  const [name, setName] = useState("");
+  const [typeId, setTypeId] = useState("");
+  const [loc, setLoc] = useState("");
+  const [stateId, setStateId] = useState(null);
+  const [cityId, setCityId] = useState(null);
+  const [street, setStreet] = useState("");
+  const [info, setInfo] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  useEffect(() => {
+    dispatch(getStates());
+    dispatch(getTypes());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (stateId) dispatch(getCities(stateId));
+  }, [dispatch, stateId]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setErrors([]);
+    const ownerId = user.id;
+    const res = await dispatch(
+      newBusiness({ name, typeId, loc, stateId, cityId, ownerId, street, info })
+    );
+
+    if (res.errors) {
+      const result = [];
+      res.errors.forEach((err) => {
+        if (err !== "Invalid value") result.push(err);
+      });
+      setErrors(result);
+    }
+    if (!res.errors) setEditBusinessModal(false);
+  };
+  return (
+    <form onSubmit={handleSubmit} className="newBusinessForm">
+      <ul className="errorsContainer">
+        {errors.map((err) => (
+          <li className="error" key={err}>
+            {err}
+          </li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={name}
+        placeholder="Name"
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <select
+        name="typeId"
+        className="newbusinessTypeBtn"
+        onChange={(e) => setTypeId(e.target.value)}
+      >
+        <option>Business Type</option>
+        {types &&
+          Object.values(types).map((type) => (
+            <option key={type.id} value={type.id}>
+              {type.name}
+            </option>
+          ))}
+      </select>
+
+      <input
+        type="text"
+        value={loc}
+        placeholder="Location"
+        onChange={(e) => setLoc(e.target.value)}
+      />
+
+      <select
+        name="stateId"
+        className="newbusinessTypeBtn"
+        onChange={(e) => setStateId(e.target.value)}
+      >
+        <option>State</option>
+        {states &&
+          Object.values(states).map((state) => (
+            <option key={state.id} value={state.id}>
+              {state.name}
+            </option>
+          ))}
+      </select>
+
+      <select
+        name="cityId"
+        className="newbusinessTypeBtn"
+        onChange={(e) => setCityId(e.target.value)}
+      >
+        <option>City</option>
+        {cities &&
+          Object.values(cities).map((city) => (
+            <option key={city.id} value={city.id}>
+              {city.name}
+            </option>
+          ))}
+      </select>
+
+      <input
+        type="text"
+        value={street}
+        placeholder="Street"
+        onChange={(e) => setStreet(e.target.value)}
+      />
+
+      <textarea
+        value={info}
+        placeholder="Business Info"
+        className="newBusinessInfo"
+        onChange={(e) => setInfo(e.target.value)}
+      />
+
+      <div className="signupBtnContainer">
+        <button className="HeaderBtnSignup">Submit</button>
+        <button
+          className="HeaderBtnSignup"
+          onClick={() => setEditBusinessModal(false)}
+        >
+          Cancel
+        </button>
+      </div>
+    </form>
+  );
+}
--- frontend/src/components/Profile/index.js
+++ frontend/src/components/Profile/index.js
@@ -1,26 +1,53 @@
-import { useEffect } from "react";
+import "./Profile.css"
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { fetchBusinesses } from "../../store/business";
+import BusinessCard from "../BusinessCard";
+import EditBusinessForm from "../EditBusinessForm";
+import Header from "../Header";
+import Modal from "../Modal";
 
 export default function Profile() {
   const dispatch = useDispatch();
   const { userId } = useParams();
   const business = useSelector((state) => state.business.businesses);
   const user = useSelector((state) => state.session.user);
+  const [editBusinessModal, setEditBusinessModal] = useState(false);
 
   useEffect(() => {
-    dispatch(fetchBusinesses({ownerId: userId}));
-  }, [business, user]);
+    dispatch(fetchBusinesses({ ownerId: userId }));
+  }, []);
   if (!business || !user) return "Loading...";
   return (
     <div>
-      {business &&
-        business.map((business) => (
-          <Link key={business.id} to={`/business/${business.id}`}>
-            {business.name}
-          </Link>
-        ))}
+      <Header />
+      <div className="businessListContentWrapper">
+        <div className="businessListContent">
+          <h1 className="businessListH1">Results</h1>
+          {business.map((card) => (
+            <div className="profileCardWrapper">
+              <BusinessCard
+                key={card.id}
+                data={card}
+                setEditBusinessModal={setEditBusinessModal}
+              />
+              <button
+                className="businessBtn"
+                onClick={() => setEditBusinessModal(true)}
+              >
+                Edit
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+      {editBusinessModal && (
+        <Modal
+          component={EditBusinessForm}
+          setEditBusinessModal={setEditBusinessModal}
+        />
+      )}
     </div>
   );
 }
